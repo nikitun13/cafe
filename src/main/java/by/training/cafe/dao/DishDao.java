7a29fcd84367de73e6e74385c4ae74bccc231201
Nikita Romanov <nikitun13@gmail.com>
2021-11-04 20:44:26 +0300

    add DishDao interface

---
+++ src/main/java/by/training/cafe/dao/DishDao.java
@@ -0,0 +1,36 @@
+package by.training.cafe.dao;
+
+import by.training.cafe.entity.Dish;
+
+import java.util.List;
+
+/**
+ * The interface {@code DishDao} is an interface that
+ * extends {@link BaseDao} and provides new operations with
+ * {@link Dish} entity.
+ *
+ * @author Nikita Romanov
+ * @see BaseDao
+ * @see Dish
+ */
+public interface DishDao extends BaseDao<Long, Dish> {
+
+    /**
+     * Finds {@code dishes} by string in the {@code name}
+     * or {@code description}.
+     *
+     * @param str string for searching.
+     * @return list of found {@code dishes}.
+     * @throws DaoException if storage access error occurs.
+     */
+    List<Dish> findByNameLikeOrDescriptionLike(String str) throws DaoException;
+
+    /**
+     * Finds {@code dishes} by {@code category}.
+     *
+     * @param category {@code category} for searching.
+     * @return list of {@code dishes} with the given {@code category}.
+     * @throws DaoException if storage access error occurs.
+     */
+    List<Dish> findByCategory(String category) throws DaoException;
+}
