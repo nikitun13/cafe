f1a03dc2a82e28361dcda0326409899181d728b0
Nikita Romanov <nikitun13@gmail.com>
2021-11-06 00:35:49 +0300

    add UserDaoImpl and tests for it (closed #1)

---
+++ src/main/java/by/training/cafe/dao/postgres/UserDaoImpl.java
@@ -0,0 +1,235 @@
+package by.training.cafe.dao.postgres;
+
+import by.training.cafe.dao.DaoException;
+import by.training.cafe.dao.UserDao;
+import by.training.cafe.entity.Language;
+import by.training.cafe.entity.User;
+import by.training.cafe.entity.UserRole;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * The class {@code CommentDaoImpl} is a class that extends
+ * {@link AbstractSqlDao} and implements {@link UserDao}.<br/>
+ * Provides access to the PostgreSQL database.
+ *
+ * @author Nikita Romanov
+ * @see AbstractSqlDao
+ * @see UserDao
+ */
+public class UserDaoImpl extends AbstractSqlDao<Long, User> implements UserDao {
+
+    private static final Logger log = LogManager.getLogger(UserDaoImpl.class);
+
+    private static final String ID_COLUMN_NAME = "id";
+    private static final String EMAIL_COLUMN_NAME = "email";
+    private static final String PASSWORD_COLUMN_NAME = "password";
+    private static final String ROLE_COLUMN_NAME = "role";
+    private static final String FIRST_NAME_COLUMN_NAME = "first_name";
+    private static final String LAST_NAME_COLUMN_NAME = "last_name";
+    private static final String PHONE_COLUMN_NAME = "phone";
+    private static final String POINTS_COLUMN_NAME = "points";
+    private static final String IS_BLOCKED_COLUMN_NAME = "is_blocked";
+    private static final String LANGUAGE_COLUMN_NAME = "language";
+
+    private static final String FIND_ALL_SQL = """
+            SELECT id, email, password, role, first_name,
+            last_name, phone, points, is_blocked, language
+            FROM users""";
+    private static final String FIND_BY_ID_SQL
+            = FIND_ALL_SQL + WHERE_SQL + "id = ?";
+    private static final String FIND_BY_EMAIL
+            = FIND_ALL_SQL + WHERE_SQL + "email = ?";
+    private static final String FIND_BY_PHONE
+            = FIND_ALL_SQL + WHERE_SQL + "phone = ?";
+    private static final String FIND_BY_ROLE
+            = FIND_ALL_SQL + WHERE_SQL + "role = ?::user_role";
+    private static final String CREATE_SQL = """
+            INSERT INTO users (email, password, role, first_name,
+            last_name, phone, points, is_blocked, language)
+            VALUES (?, ?, ?::user_role, ?, ?, ?, ?, ?, ?::app_language)""";
+    private static final String UPDATE_SQL = """
+            UPDATE users
+            SET email      = ?,
+                password   = ?,
+                role       = ?::user_role,
+                first_name = ?,
+                last_name  = ?,
+                phone      = ?,
+                points     = ?,
+                is_blocked = ?,
+                language   = ?::app_language
+            WHERE id = ?""";
+    private static final String DELETE_SQL = """
+            DELETE FROM users
+            WHERE id = ?""";
+
+    public UserDaoImpl(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    public List<User> findAll() throws DaoException {
+        List<User> users = executeSelectQuery(
+                FIND_ALL_SQL, Collections.emptyList());
+        log.debug(RESULT_LOG_MESSAGE, users);
+        return users;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) throws DaoException {
+        log.debug("Received id: {}", id);
+        List<User> list = executeSelectQuery(
+                FIND_BY_ID_SQL, List.of(id));
+        Optional<User> maybeUser = getFirstEntityFromList(list);
+        log.debug(RESULT_LOG_MESSAGE, maybeUser);
+        return maybeUser;
+    }
+
+    @Override
+    public void create(User entity) throws DaoException {
+        log.debug("Received user: {}", entity);
+        List<Object> params = createParamsList(entity);
+        log.debug("{} params for query: {}", entity, params);
+        Optional<Long> maybeId = executeCreateQuery(
+                CREATE_SQL, Long.class, params);
+        maybeId.ifPresentOrElse(
+                entity::setId,
+                () -> log.error("No generated keys for {}", entity)
+        );
+        log.debug("{} was created in db", entity);
+    }
+
+    @Override
+    public boolean update(User entity) throws DaoException {
+        log.debug("Received user: {}", entity);
+        List<Object> params = createParamsList(entity);
+        params.add(entity.getId());
+        log.debug("{} params for query: {}", entity, params);
+        int updatedRows = executeUpdateQuery(UPDATE_SQL, params);
+        boolean isUpdated = isOnlyOneRowUpdated(updatedRows);
+        if (isUpdated) {
+            log.debug("{} was updated", entity);
+        } else {
+            log.warn("{} wasn't updated", entity);
+        }
+        return isUpdated;
+    }
+
+    @Override
+    public boolean delete(Long id) throws DaoException {
+        log.debug("Received id: {}", id);
+        int updatedRows = executeUpdateQuery(DELETE_SQL, List.of(id));
+        boolean isDeleted = isOnlyOneRowUpdated(updatedRows);
+        if (isDeleted) {
+            log.debug("User with id {} was deleted", id);
+        } else {
+            log.warn("User with id {} wasn't deleted", id);
+        }
+        return isDeleted;
+    }
+
+    @Override
+    public Optional<User> findByPhone(String phone) throws DaoException {
+        log.debug("Received phone: {}", phone);
+        List<User> list = executeSelectQuery(FIND_BY_PHONE, List.of(phone));
+        Optional<User> maybeUser = getFirstEntityFromList(list);
+        log.debug(RESULT_LOG_MESSAGE, maybeUser);
+        return maybeUser;
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) throws DaoException {
+        log.debug("Received email: {}", email);
+        List<User> list = executeSelectQuery(FIND_BY_EMAIL, List.of(email));
+        Optional<User> maybeUser = getFirstEntityFromList(list);
+        log.debug(RESULT_LOG_MESSAGE, maybeUser);
+        return maybeUser;
+    }
+
+    @Override
+    public List<User> findByRole(String role) throws DaoException {
+        log.debug("Received role: {}", role);
+        List<User> users = executeSelectQuery(FIND_BY_ROLE, List.of(role));
+        log.debug(RESULT_LOG_MESSAGE, users);
+        return users;
+    }
+
+    @Override
+    protected User buildEntity(ResultSet resultSet) throws SQLException {
+        Long id = resultSet.getObject(
+                ID_COLUMN_NAME, Long.class);
+        log.trace("id = {}", id);
+
+        String email = resultSet.getObject(
+                EMAIL_COLUMN_NAME, String.class);
+        log.trace("email = {}", email);
+
+        String password = resultSet.getObject(
+                PASSWORD_COLUMN_NAME, String.class);
+        log.trace("password = {}", password);
+
+        UserRole role = UserRole.valueOf(resultSet.getObject(
+                ROLE_COLUMN_NAME, String.class));
+        log.trace("role = {}", role);
+
+        String firstName = resultSet.getObject(
+                FIRST_NAME_COLUMN_NAME, String.class);
+        log.trace("firstName = {}", firstName);
+
+        String lastName = resultSet.getObject(
+                LAST_NAME_COLUMN_NAME, String.class);
+        log.trace("lastName = {}", lastName);
+
+        String phone = resultSet.getObject(
+                PHONE_COLUMN_NAME, String.class);
+        log.trace("phone = {}", phone);
+
+        Long points = resultSet.getObject(
+                POINTS_COLUMN_NAME, Long.class);
+        log.trace("points = {}", points);
+
+        Boolean isBlocked = resultSet.getObject(
+                IS_BLOCKED_COLUMN_NAME, Boolean.class);
+        log.trace("isBlocked = {}", isBlocked);
+
+        Language language = Language.valueOf(resultSet.getObject(
+                LANGUAGE_COLUMN_NAME, String.class));
+        log.trace("language = {}", language);
+
+        return User.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .role(role)
+                .firstName(firstName)
+                .lastName(lastName)
+                .phone(phone)
+                .points(points)
+                .isBlocked(isBlocked)
+                .language(language)
+                .build();
+    }
+
+    private List<Object> createParamsList(User user) {
+        List<Object> params = new ArrayList<>();
+        params.add(user.getEmail());
+        params.add(user.getPassword());
+        params.add(user.getRole().toString());
+        params.add(user.getFirstName());
+        params.add(user.getLastName());
+        params.add(user.getPhone());
+        params.add(user.getPoints());
+        params.add(user.isBlocked());
+        params.add(user.getLanguage().toString());
+        return params;
+    }
+}
---
+++ src/test/java/by/training/cafe/dao/postgres/UserDaoImplTest.java
@@ -0,0 +1,331 @@
+package by.training.cafe.dao.postgres;
+
+import by.training.cafe.dao.DaoException;
+import by.training.cafe.dao.pool.ConnectionPool;
+import by.training.cafe.entity.Language;
+import by.training.cafe.entity.User;
+import by.training.cafe.entity.UserRole;
+import by.training.cafe.extension.DatabaseExtension;
+import by.training.cafe.extension.SqlDaoParamResolver;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({
+        DatabaseExtension.class,
+        SqlDaoParamResolver.class
+})
+class UserDaoImplTest {
+
+    private static final String ID_COLUMN_NAME = "id";
+    private static final String EMAIL_COLUMN_NAME = "email";
+    private static final String PASSWORD_COLUMN_NAME = "password";
+    private static final String ROLE_COLUMN_NAME = "role";
+    private static final String FIRST_NAME_COLUMN_NAME = "first_name";
+    private static final String LAST_NAME_COLUMN_NAME = "last_name";
+    private static final String PHONE_COLUMN_NAME = "phone";
+    private static final String POINTS_COLUMN_NAME = "points";
+    private static final String IS_BLOCKED_COLUMN_NAME = "is_blocked";
+    private static final String LANGUAGE_COLUMN_NAME = "language";
+
+    private static final String SET_UP_SQL = """
+            INSERT INTO users (id, email, password, role, first_name, last_name, phone, points, is_blocked, language)
+            VALUES (1000000, 'ivan@gmail.com', '$2a$10$.IVAN.PASS.43WRAZ3Fnyx.C/6PveEHf6JGzGo9X2SQSwM5djXdrO', 'CLIENT', 'Ivan', 'Melnikov','+375251111111',30,TRUE,'RU'),
+                   (1000001, 'petr@mail.ru', '$2a$10$.ПЕТР.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr1', 'CLIENT', 'Петр', 'Шариков','+375442222222',130,FALSE,'RU'),
+                   (1000002, 'john@gmail.com', '$2a$10$.John.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr2', 'ADMIN', 'John', 'Henson','+375333333333',270,FALSE,'EN'),
+                   (1000003, 'hans@gmail.com', '$2a$10$.Hans.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr3', 'CLIENT', 'Hans', 'Münz','+375254444444',0,FALSE,'DE')""";
+    private static final String TEAR_DOWN_SQL = "DELETE FROM users";
+    private static final String FIND_BY_ID_SQL = "SELECT * FROM users WHERE id = ?";
+
+    private static final Connection connection = ConnectionPool.getInstance().getConnection();
+    private static final User ivan;
+    private static final User petr;
+    private static final User john;
+
+    static {
+        ivan = User.builder()
+                .id(1_000_000L)
+                .email("ivan@gmail.com")
+                .password("$2a$10$.IVAN.PASS.43WRAZ3Fnyx.C/6PveEHf6JGzGo9X2SQSwM5djXdrO")
+                .role(UserRole.CLIENT)
+                .firstName("Ivan")
+                .lastName("Melnikov")
+                .phone("+375251111111")
+                .points(30L)
+                .isBlocked(true)
+                .language(Language.RU)
+                .build();
+
+        petr = User.builder()
+                .id(1_000_001L)
+                .email("petr@mail.ru")
+                .password("$2a$10$.ПЕТР.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr1")
+                .role(UserRole.CLIENT)
+                .firstName("Петр")
+                .lastName("Шариков")
+                .phone("+375442222222")
+                .points(130L)
+                .isBlocked(false)
+                .language(Language.RU)
+                .build();
+
+        john = User.builder()
+                .id(1_000_002L)
+                .email("john@gmail.com")
+                .password("$2a$10$.John.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr2")
+                .role(UserRole.ADMIN)
+                .firstName("John")
+                .lastName("Henson")
+                .phone("+375333333333")
+                .points(270L)
+                .isBlocked(false)
+                .language(Language.EN)
+                .build();
+    }
+
+    private final User hans;
+    private final User adam;
+
+    private final UserDaoImpl userDao;
+
+    UserDaoImplTest(UserDaoImpl userDao) {
+        this.userDao = userDao;
+
+        hans = User.builder()
+                .id(1_000_003L)
+                .email("hans@gmail.com")
+                .password("$2a$10$.Hans.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr3")
+                .role(UserRole.CLIENT)
+                .firstName("Hans")
+                .lastName("Münz")
+                .phone("+375254444444")
+                .points(0L)
+                .isBlocked(false)
+                .language(Language.DE)
+                .build();
+
+        adam = User.builder()
+                .email("smith@gmail.com")
+                .password("$2a$10$.Adam.PASS.43NRAZ3Fny.C/6PveEH6JGzGo9X2SQSwM5djXwpdr4")
+                .role(UserRole.CLIENT)
+                .firstName("Adam")
+                .lastName("Smith")
+                .phone("+375445555555")
+                .points(500L)
+                .isBlocked(false)
+                .language(Language.EN)
+                .build();
+    }
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(SET_UP_SQL)) {
+            statement.executeUpdate();
+        }
+    }
+
+    @Test
+    @Tag("findAll")
+    void shouldReturnAllUsersFromDatabase() throws DaoException {
+        List<User> expected = List.of(ivan, petr, john, hans);
+
+        List<User> actual = userDao.findAll();
+
+        assertEquals(expected, actual, () -> "Must return list: " + expected);
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldReturnExistingUserById() throws DaoException {
+        User expected = john;
+
+        Optional<User> optionalUser = userDao.findById(1_000_002L);
+
+        optionalUser.ifPresentOrElse(
+                (actual) -> assertEquals(expected, actual, () -> "expected to get:" + expected),
+                Assertions::fail
+        );
+    }
+
+    @Test
+    @Tag("findById")
+    void shouldReturnEmptyOptionalIfThereIsNoSuchUserId() throws DaoException {
+        Optional<User> maybeActual = userDao.findById(-1L);
+
+        assertTrue(maybeActual.isEmpty());
+    }
+
+    @Test
+    @Tag("create")
+    void shouldCreatedUserInDatabase() throws DaoException, SQLException {
+        User expected = adam;
+
+        userDao.create(expected);
+        User actual = findById(expected.getId());
+
+        assertEquals(expected, actual, () -> "must create: " + expected);
+    }
+
+    @Test
+    @Tag("update")
+    void shouldUpdateUserInDatabase() throws DaoException, SQLException {
+        User expected = hans;
+        expected.setPoints(319L);
+        expected.setPhone("+49302291110");
+
+        boolean isUpdated = userDao.update(expected);
+        User actual = findById(expected.getId());
+        assertAll(
+                () -> assertTrue(isUpdated, "must return true if user was updated in the database"),
+                () -> assertEquals(expected, actual, () -> "must update in database to: " + expected)
+        );
+    }
+
+    @Test
+    @Tag("update")
+    void shouldReturnFalseIfUserWasNotUpdated() throws DaoException {
+        adam.setId(-1L);
+        boolean isUpdated = userDao.update(adam);
+
+        assertFalse(isUpdated, "mustn't update any users if no such user id");
+    }
+
+    @Test
+    @Tag("delete")
+    void delete() throws DaoException, SQLException {
+        User expected = hans;
+
+        boolean isDeleted = userDao.delete(expected.getId());
+        User actual = findById(expected.getId());
+
+        assertAll(
+                () -> assertTrue(isDeleted, "must return true if user was deleted from database"),
+                () -> assertNull(actual, () -> "must delete %s from database".formatted(expected))
+        );
+    }
+
+    @Test
+    @Tag("delete")
+    void shouldReturnFalseIfUserWasNotDeleted() throws DaoException {
+        boolean isDeleted = userDao.delete(-1L);
+
+        assertFalse(isDeleted, "mustn't delete any users if no such user id");
+    }
+
+    @Test
+    @Tag("findByPhone")
+    void shouldReturnUserByPhone() throws DaoException {
+        User expected = ivan;
+
+        Optional<User> optionalUser = userDao.findByPhone("+375251111111");
+
+        optionalUser.ifPresentOrElse(
+                (actual) -> assertEquals(expected, actual, () -> "expected to get:" + expected),
+                Assertions::fail
+        );
+    }
+
+    @Test
+    @Tag("findByPhone")
+    void shouldReturnEmptyOptionalIfThereIsNoPhone() throws DaoException {
+        Optional<User> maybeActual = userDao.findByPhone("+375000000000");
+
+        assertTrue(maybeActual.isEmpty());
+    }
+
+    @Test
+    @Tag("findByEmail")
+    void findByEmail() throws DaoException {
+        User expected = petr;
+
+        Optional<User> optionalUser = userDao.findByEmail("petr@mail.ru");
+
+        optionalUser.ifPresentOrElse(
+                (actual) -> assertEquals(expected, actual, () -> "expected to get:" + expected),
+                Assertions::fail
+        );
+    }
+
+    @Test
+    @Tag("findByEmail")
+    void shouldReturnEmptyOptionalIfThereIsEmail() throws DaoException {
+        Optional<User> maybeActual = userDao.findByEmail("no@such.email");
+
+        assertTrue(maybeActual.isEmpty());
+    }
+
+    @Test
+    @Tag("findByRole")
+    void shouldReturnAllClients() throws DaoException {
+        List<User> expected = List.of(ivan, petr, hans);
+
+        List<User> actual = userDao.findByRole("CLIENT");
+
+        assertEquals(expected, actual, () -> "Must return list: " + expected);
+    }
+
+    @Test
+    @Tag("findByRole")
+    void shouldReturnAllAdmins() throws DaoException {
+        List<User> expected = List.of(john);
+
+        List<User> actual = userDao.findByRole("ADMIN");
+
+        assertEquals(expected, actual, () -> "Must return list: " + expected);
+    }
+
+    @Test
+    @Tag("findByRole")
+    void shouldThrowExceptionIfThereIsNoSuchRole() {
+        assertThrows(DaoException.class,
+                () -> userDao.findByRole("noSuchRole"),
+                () -> "Must throw %s if there is no such role".formatted(DaoException.class.getName()));
+    }
+
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(TEAR_DOWN_SQL)) {
+            statement.executeUpdate();
+        }
+    }
+
+    @AfterAll
+    static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private User findById(Long id) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            statement.setObject(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return User.builder()
+                        .id(resultSet.getObject(ID_COLUMN_NAME, Long.class))
+                        .email(resultSet.getObject(EMAIL_COLUMN_NAME, String.class))
+                        .password(resultSet.getObject(PASSWORD_COLUMN_NAME, String.class))
+                        .role(UserRole.valueOf(resultSet.getObject(ROLE_COLUMN_NAME, String.class)))
+                        .firstName(resultSet.getObject(FIRST_NAME_COLUMN_NAME, String.class))
+                        .lastName(resultSet.getObject(LAST_NAME_COLUMN_NAME, String.class))
+                        .phone(resultSet.getObject(PHONE_COLUMN_NAME, String.class))
+                        .points(resultSet.getObject(POINTS_COLUMN_NAME, Long.class))
+                        .isBlocked(resultSet.getObject(IS_BLOCKED_COLUMN_NAME, Boolean.class))
+                        .language(Language.valueOf(resultSet.getObject(LANGUAGE_COLUMN_NAME, String.class)))
+                        .build();
+            }
+        }
+        return null;
+    }
+}
