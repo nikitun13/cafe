f85a01607107201864cdf61f0777427ff793ee62
Nikita Romanov <nikitun13@gmail.com>
2021-11-17 18:26:02 +0300

    add SessionListener

---
+++ src/main/java/by/training/cafe/controller/listener/SessionListenerImpl.java
@@ -0,0 +1,51 @@
+package by.training.cafe.controller.listener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener
+public class SessionListenerImpl implements HttpSessionAttributeListener {
+
+    private static final Logger log
+            = LogManager.getLogger(SessionListenerImpl.class);
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent event) {
+        String sessionId = event.getSession().getId();
+        String name = event.getName();
+        Object value = event.getValue();
+
+        log.debug("Attribute '{}' with value = '{}'"
+                        + " was added to session with id = {}",
+                name, value, sessionId);
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent event) {
+        String sessionId = event.getSession().getId();
+        String name = event.getName();
+        Object value = event.getValue();
+
+        log.debug("Attribute '{}' with value = '{}'"
+                        + " was removed from session with id = {}",
+                name, value, sessionId);
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent event) {
+        String name = event.getName();
+        if (name.equals("javax.servlet.jsp.jstl.fmt.request.charset")) {
+            return;
+        }
+        String sessionId = event.getSession().getId();
+        Object value = event.getValue();
+
+        log.debug("Attribute '{}' was replaced with value = '{}'."
+                        + " Session id = {}",
+                name, value, sessionId);
+    }
+}
