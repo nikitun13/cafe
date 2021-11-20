a855f7803f43248803fcb76fb0c60c280f395ee5
Nikita Romanov <nikitun13@gmail.com>
2021-11-20 22:32:01 +0300

    update RequestListenerImpl

--- src/main/java/by/training/cafe/controller/listener/RequestListenerImpl.java
+++ src/main/java/by/training/cafe/controller/listener/RequestListenerImpl.java
@@ -18,12 +18,13 @@ public class RequestListenerImpl implements ServletRequestListener {
     public void requestInitialized(ServletRequestEvent sre) {
         HttpServletRequest request = (HttpServletRequest) sre.getServletRequest();
         String requestURI = request.getRequestURI();
+        String method = request.getMethod();
         if (!requestURI.startsWith("/img/")
                 && !requestURI.startsWith("/css/")
                 && !requestURI.startsWith("/js/")) {
             String sessionId = request.getSession().getId();
-            log.debug("Session id = {}. Requested URI: {}",
-                    sessionId, requestURI);
+            log.debug("Session id = {}. Requested URI: {} method: {}",
+                    sessionId, requestURI, method);
         }
     }
 }
