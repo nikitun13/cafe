ecffc2c69bdf2931fa11324bee96e7a34003bdd0
Nikita Romanov <nikitun13@gmail.com>
2021-11-21 20:20:40 +0300

    add CommonAttributes

---
+++ src/main/java/by/training/cafe/controller/command/CommonAttributes.java
@@ -0,0 +1,20 @@
+package by.training.cafe.controller.command;
+
+public final class CommonAttributes {
+
+    public static final String ERROR_STATUS = "errorStatus";
+    public static final String ERROR_MESSAGE_KEY = "errorMessageKey";
+    public static final String LAST_PAGE = "lastPage";
+    public static final String USER = "user";
+    public static final String IS_ERROR_OCCURRED = "isErrorOccurred";
+    public static final String SUCCESS_MESSAGE_KEY = "successMessageKey";
+    public static final String FIRST_NAME = "firstName";
+    public static final String LAST_NAME = "lastName";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String REPEAT_PASSWORD = "repeatPassword";
+    public static final String PHONE = "phone";
+
+    private CommonAttributes() {
+    }
+}
