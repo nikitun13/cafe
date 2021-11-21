078b09731e206548f11a3917bf7e840d95afd6a4
Nikita Romanov <nikitun13@gmail.com>
2021-11-21 09:39:30 +0300

    add UnknownCommand

---
+++ src/main/java/by/training/cafe/controller/command/impl/UnknownCommand.java
@@ -0,0 +1,39 @@
+package by.training.cafe.controller.command.impl;
+
+import by.training.cafe.controller.command.Command;
+import by.training.cafe.controller.command.Dispatch;
+import by.training.cafe.util.JspPathUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.HttpURLConnection;
+
+/**
+ * The class {@code UnknownCommand} is a class that
+ * implements {@link Command}.<br/>
+ * Executes when an unknown command is received.
+ *
+ * @author Nikita Romanov
+ * @see Command
+ */
+public class UnknownCommand implements Command {
+
+    private static final Dispatch RESULT = new Dispatch(
+            Dispatch.DispatchType.FORWARD,
+            JspPathUtil.getPath("error"));
+    private static final String ERROR_MESSAGE_ATTRIBUTE_KEY
+            = "errorMessageKey";
+    private static final String ERROR_STATUS_ATTRIBUTE_KEY
+            = "errorStatus";
+
+    @Override
+    public Dispatch execute(HttpServletRequest request,
+                            HttpServletResponse response) {
+        response.setStatus(HttpURLConnection.HTTP_NOT_FOUND);
+        request.setAttribute(ERROR_STATUS_ATTRIBUTE_KEY,
+                HttpURLConnection.HTTP_NOT_FOUND);
+        request.setAttribute(ERROR_MESSAGE_ATTRIBUTE_KEY,
+                "main.nothingFound");
+        return RESULT;
+    }
+}
